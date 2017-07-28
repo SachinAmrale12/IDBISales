c68ef634aaf9b94f9c869bbdf4f708b6d3daa5e1
Sachin Amrale <sachinamrale@Sachins-MacBook-Air.local>
2017-07-28 17:26:31 +0530

    add login view controller in storyboard

--- IDBISales/ViewController.swift
+++ IDBISales/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userIdTextField          : RaisePlaceholder!
+    @IBOutlet weak var passwordTextField        : RaisePlaceholder!
+    @IBOutlet weak var loginButton              : UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.commonInitialization()
+        
     }
 
+    func commonInitialization()
+    {
+        userIdTextField.animationDuration = 0.5
+        userIdTextField.subjectColor = UIColor.orange
+        userIdTextField.underLineColor = UIColor.orange
+        
+        passwordTextField.animationDuration = 0.5
+        passwordTextField.subjectColor = UIColor.orange
+        passwordTextField.underLineColor = UIColor.orange
+        
+        loginButton.layer.cornerRadius = loginButton.frame.size.height / 2
+        loginButton.layer.borderColor = UIColor.orange.cgColor
+        loginButton.layer.borderWidth = 1
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
